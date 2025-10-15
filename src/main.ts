82c30104caf1b653a5641bd82b16d329d67c7555
Tate Maguire <tatemaguir@gmail.com>
2025-10-15 16:52:53 -0700

    implemented drawing

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,21 @@ import "./style.css";
 
 document.body.innerHTML = `
   <h1>DRAW!</h1>
-  <canvas width="256" height="256"></canvas>
+  <canvas id="canvas" width="256" height="256"></canvas>
 `;
+
+const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+
+canvas.addEventListener("mousedown", (e) => {
+  ctx.beginPath();
+  ctx.moveTo(e.offsetX, e.offsetY);
+  console.log(e.offsetX, e.offsetY);
+});
+
+canvas.addEventListener("mousemove", (e) => {
+  if (e.buttons & 1) {
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+  }
+});
