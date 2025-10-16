bd7d76a3db66748a5254464e839681c49f17442d
Tate Maguire <tatemaguir@gmail.com>
2025-10-16 16:43:22 -0700

    refactored using display-changed event and array of lines (STEP 3)

--- src/main.ts
+++ src/main.ts
@@ -9,14 +9,34 @@ document.body.innerHTML = `
 const canvas = document.getElementById("canvas") as HTMLCanvasElement;
 const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
 
+interface Point {
+  x: number;
+  y: number;
+}
+
+const lines: Point[][] = [];
+
 canvas.addEventListener("mousedown", (e) => {
-  ctx.beginPath();
-  ctx.moveTo(e.offsetX, e.offsetY);
+  const line: Point[] = [];
+  line.push({ x: e.offsetX, y: e.offsetY });
+  lines.push(line);
 });
 
 canvas.addEventListener("mousemove", (e) => {
   if (e.buttons & 1) {
-    ctx.lineTo(e.offsetX, e.offsetY);
+    const line = lines[lines.length - 1];
+    line.push({ x: e.offsetX, y: e.offsetY });
+    canvas.dispatchEvent(new Event("display-changed"));
+  }
+});
+
+canvas.addEventListener("display-changed", () => {
+  for (const line of lines) {
+    ctx.beginPath();
+    ctx.moveTo(line[0].x, line[0].y);
+    for (const pt of line) {
+      ctx.lineTo(pt.x, pt.y);
+    }
     ctx.stroke();
   }
 });
@@ -25,4 +45,5 @@ const clearButton = document.getElementById("clear") as HTMLButtonElement;
 
 clearButton.addEventListener("click", () => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+  lines.splice(0, lines.length);
 });
