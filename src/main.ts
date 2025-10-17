4753f24b03df896073dfde6f0ed12467a19ce4b7
Tate Maguire <tatemaguir@gmail.com>
2025-10-16 17:02:54 -0700

    implemented undo and redo (STEP 4)

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,10 @@ import "./style.css";
 document.body.innerHTML = `
   <h1>DRAW!</h1>
   <canvas id="canvas" width="256" height="256"></canvas>
+  <br/><br/>
   <button id="clear">Clear</button>
+  <button id="undo">Undo</button>
+  <button id="redo">Redo</button>
 `;
 
 const canvas = document.getElementById("canvas") as HTMLCanvasElement;
@@ -14,23 +17,26 @@ interface Point {
   y: number;
 }
 
-const lines: Point[][] = [];
+const lines: Point[][] = []; // tail element is most recently drawn
+const redoLines: Point[][] = []; // tail element is most recently undone
 
 canvas.addEventListener("mousedown", (e) => {
   const line: Point[] = [];
   line.push({ x: e.offsetX, y: e.offsetY });
   lines.push(line);
+  redoLines.splice(0, redoLines.length);
 });
 
 canvas.addEventListener("mousemove", (e) => {
   if (e.buttons & 1) {
     const line = lines[lines.length - 1];
     line.push({ x: e.offsetX, y: e.offsetY });
-    canvas.dispatchEvent(new Event("display-changed"));
+    notify("display-changed");
   }
 });
 
 canvas.addEventListener("display-changed", () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
   for (const line of lines) {
     ctx.beginPath();
     ctx.moveTo(line[0].x, line[0].y);
@@ -41,9 +47,34 @@ canvas.addEventListener("display-changed", () => {
   }
 });
 
+function notify(message: string) {
+  canvas.dispatchEvent(new Event(message));
+}
+
 const clearButton = document.getElementById("clear") as HTMLButtonElement;
 
 clearButton.addEventListener("click", () => {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
   lines.splice(0, lines.length);
+  redoLines.splice(0, redoLines.length);
+  notify("display-changed");
+});
+
+const undoButton = document.getElementById("undo") as HTMLButtonElement;
+
+undoButton.addEventListener("click", () => {
+  const undoneLine = lines.pop();
+  if (undoneLine) {
+    redoLines.push(undoneLine);
+    notify("display-changed");
+  }
+});
+
+const redoButton = document.getElementById("redo") as HTMLButtonElement;
+
+redoButton.addEventListener("click", () => {
+  const redoneLine = redoLines.pop();
+  if (redoneLine) {
+    lines.push(redoneLine);
+    notify("display-changed");
+  }
 });
