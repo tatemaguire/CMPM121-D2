63d813c4a13dd15e8ebbb7a9b06750b9fb24b882
Tate Maguire <tatemaguir@gmail.com>
2025-10-21 20:31:03 -0700

    added ToolPreview class that displays the tool under the cursor. added tool-moved event for when the cursor moves (STEP 7)

--- src/main.ts
+++ src/main.ts
@@ -11,8 +11,9 @@ document.body.innerHTML = `
   <button id="makeThick">Thick Stroke</button>
 `;
 
-const canvas = document.getElementById("canvas") as HTMLCanvasElement;
-const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+// ----------------------------------------------------------
+// ----------------- Interfaces and classes -----------------
+// ----------------------------------------------------------
 
 interface Point {
   x: number;
@@ -44,15 +45,48 @@ class MarkerLine implements Displayable {
   }
 }
 
+class ToolPreview implements Displayable {
+  cursor: Point;
+  constructor(cursor: Point) {
+    this.cursor = cursor;
+  }
+  display(context: CanvasRenderingContext2D) {
+    context.beginPath();
+    const radius = (currentlyThick ? 10 : 2) / 2;
+    context.arc(this.cursor.x, this.cursor.y, radius, 0, 2 * Math.PI);
+    context.fill();
+  }
+}
+
+// ----------------------------------------------------------
+// ----------------- Global Variables -----------------------
+// ----------------------------------------------------------
+
+const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+
 const redoObjects: Displayable[] = [];
 const displayObjects: Displayable[] = [];
 let currentLine: MarkerLine | null = null;
 let currentlyThick = false;
+let toolPreview: ToolPreview | null = null;
+
+// ----------------------------------------------------------
+// ----------------- Canvas Mouse Events --------------------
+// ----------------------------------------------------------
 
 canvas.addEventListener("mousedown", (e) => {
   redoObjects.splice(0, redoObjects.length);
   currentLine = new MarkerLine({ x: e.offsetX, y: e.offsetY }, currentlyThick);
   displayObjects.push(currentLine);
+
+  toolPreview = null;
+  notify("tool-moved");
+});
+
+canvas.addEventListener("mouseup", (e) => {
+  toolPreview = new ToolPreview({ x: e.offsetX, y: e.offsetY });
+  notify("tool-moved");
 });
 
 canvas.addEventListener("mousemove", (e) => {
@@ -63,18 +97,44 @@ canvas.addEventListener("mousemove", (e) => {
     });
     notify("display-changed");
   }
+  if (toolPreview) {
+    toolPreview.cursor = { x: e.offsetX, y: e.offsetY };
+    notify("tool-moved");
+  }
 });
 
-canvas.addEventListener("display-changed", () => {
+canvas.addEventListener("mouseenter", (e) => {
+  toolPreview = new ToolPreview({ x: e.offsetX, y: e.offsetY });
+  notify("tool-moved");
+});
+
+canvas.addEventListener("mouseleave", () => {
+  toolPreview = null;
+  notify("tool-moved");
+});
+
+// ----------------------------------------------------------
+// ------------- Custom events and notifications ------------
+// ----------------------------------------------------------
+
+function redraw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   for (const obj of displayObjects) {
     obj.display(ctx);
   }
-});
+  toolPreview?.display(ctx);
+}
+
+canvas.addEventListener("display-changed", redraw);
+canvas.addEventListener("tool-moved", redraw);
 
 function notify(message: string) {
   canvas.dispatchEvent(new Event(message));
 }
+
+// ----------------------------------------------------------
+// ----------------- Button Callbacks -----------------------
+// ----------------------------------------------------------
 
 const clearButton = document.getElementById("clear") as HTMLButtonElement;
 
